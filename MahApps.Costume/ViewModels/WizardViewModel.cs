e5861ed5429c1787d2489a50d843d7c55baa702b
frankwwu <frankwwu@hotmail.com>
2018-06-23 11:01:38 -0400

    Handling of Next and Finish events

--- MahApps.Costume/ViewModels/WizardViewModel.cs
+++ MahApps.Costume/ViewModels/WizardViewModel.cs
@@ -1,9 +1,17 @@
 ﻿using MahAppsDemo.Mvvm;
+using Xceed.Wpf.Toolkit.Core;
 
 namespace MahAppsDemo.ViewModels
 {
     public class WizardViewModel : BindableBase
     {
+
+        public WizardViewModel()
+        {
+            NextCommand = new DelegateCommand<object>(Next);
+            FinishCommand = new DelegateCommand<object>(Finish);
+        }
+
         public bool CanNextFromPage1
         {
             get
@@ -19,5 +27,27 @@ namespace MahAppsDemo.ViewModels
                 return true;
             }
         }
+
+        #region NextCommand
+
+        public DelegateCommand<object> NextCommand { get; private set; }
+
+        public void Next(object parameter)
+        {
+            CancelRoutedEventArgs args = parameter as CancelRoutedEventArgs;
+        }
+
+        #endregion NextCommand
+
+        #region FinishCommand
+
+        public DelegateCommand<object> FinishCommand { get; private set; }
+
+        public void Finish(object parameter)
+        {
+            CancelRoutedEventArgs args = parameter as CancelRoutedEventArgs;
+        }
+
+        #endregion FinishCommand
     }
 }
