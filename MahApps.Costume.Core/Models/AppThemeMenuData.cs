f7677bbf522c0f55b0816b9cd675f9b01525dc56
frankwwu <frankwwu@hotmail.com>
2021-02-23 17:28:30 -0500

    MahApps.Costume.Core

---
+++ MahApps.Costume.Core/Models/AppThemeMenuData.cs
@@ -0,0 +1,24 @@
+﻿using ControlzEx.Theming;
+using MahApps.Costume.Properties;
+using System.Windows;
+
+namespace MahApps.Costume.Models
+{
+    public class AppThemeMenuData : AccentColorMenuData
+    {
+        public AppThemeMenuData()
+        {
+            if (!string.IsNullOrEmpty(Settings.Default.Theme))
+            {
+                ThemeManager.Current.ChangeThemeBaseColor(Application.Current, Settings.Default.Theme);
+            }
+        }
+
+        protected override void ChangeTheme(object sender)
+        {
+            ThemeManager.Current.ChangeThemeBaseColor(Application.Current, this.Name);
+            Settings.Default.Theme = this.Name;
+            Settings.Default.Save();
+        }
+    }
+}
