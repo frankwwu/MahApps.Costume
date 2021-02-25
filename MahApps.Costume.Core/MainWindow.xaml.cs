da01c8cdbe30eaddccb0fffd4705978f340a7720
frankwwu <frankwwu@hotmail.com>
2021-02-25 08:50:34 -0500

    MahApps.Costume.Core

---
+++ MahApps.Costume.Core/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MahApps.Costume.ViewModels;
+using MahApps.Metro.Controls;
+
+namespace MahApps.Costume
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+    }
+}
